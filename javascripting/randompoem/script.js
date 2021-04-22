385f60e88f3debe239c7707a5d120d96cb5b078c
urechioguguo <uo2123@columbia.edu>
2021-04-22 16:08:04 -0400

    update

--- javascripting/randompoem/script.js
+++ javascripting/randompoem/script.js
@@ -70,6 +70,8 @@ let array_one = [
       `${element_from_array_one} \n ${element_from_array_two} \n ${element_from_array_three}`
     );
   }
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
   
   let btn = document.querySelector("button");
   btn.addEventListener("click", renderpoem);
